c3f65c5fec4729e09c697d075d3b2cecb03af45a
James Cryer <james.cryer@huddle.net>
2012-10-02 17:19:44 +0100

    First stab at open-sourcing Huddle's Sinon/Phantom module

--- README.md
+++ README.md
@@ -2,3 +2,57 @@ PhantomXHR
 ==========
 
 Integration of SinonJS's mock XHR interface with PhantomJS
+
+## huddle.xhr.fake
+
+### The good stuff
+
+* [Controlling response sequences](#sequences)
+* [Getters for making assertions](#getters)
+
+### Overview
+
+Isolating the UI from the API requires the insertion of mocks to simulate server responses. To setup a mock you must specify a URI to match on and a response.  The URL can either be a string or a regular expression.  The response body (in our case) is a JSON object.
+
+```javascript
+
+var xhr = require('./modules/PhantomXHR/phantomxhr.js);
+
+xhr.fake({
+  url: /documents\/([0-9]+\?)/,
+  responseBody: { /*my response*/ }
+});
+```
+
+In the above case, if an API call to 'something/documents/48546?' is made, the JavaScript application will receive the response.  The JS app with then use this fake data to build up the UI.
+
+Note: be careful when defining URL matches, try to keep them specific otherwise you may find that the wrong XHR fake is responding.
+
+### <a name="sequences"></a> Response sequences
+
+Your mock will return an object which exposes some useful methods for changing subsequent responses.  This allows us to test retry user flows; say you want to test that a retry button appears after a 500, trying again and getting a 200 should remove the retry button.
+
+```javascript
+var deleteRequest = xhr.fake({
+  url: /documents\/([0-9]+\?)/,
+  method: 'delete',
+  status: 500
+});
+
+deleteRequest.nthResponse(2,{
+  status: 200
+});
+```
+
+This method will also let you return a different responseBody on a subsequent request.
+
+### <a name="getters"></a> Getters
+
+Your fake response will expose the following methods to allow you to make all kinds of test assertions.
+
+```javascript
+deleteRequest.first(); // get the first requestBody it intercepted
+deleteRequest.last(); // get the last requestBody it intercepted
+deleteRequest.nthRequest(6); // get the 6th requestBody it intercepted
+deleteRequest.count(); // get the number of requests made
+```
---
+++ phantomxhr.js
@@ -0,0 +1,212 @@
+exports.init = init;
+exports.fake = fake;
+exports.requests = getAllRequests;
+
+var page;
+
+function init(initPage){
+	var inject = initPage.injectJs('./modules/PhantomXHR/sinon.js');
+
+	page = initPage;
+
+	if(inject){
+		setup();
+	} else {
+		console.log("Can't find sinon.js");
+	}
+}
+
+function setup(){
+	page.evaluate(function () {
+
+		if (!window._ajaxmock_) {
+			window._ajaxmock_ = {
+				matches: [],
+				requests: {},
+				call: {},
+				fake: function (match) {
+
+					function S4() {
+						return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+					}
+
+					function makeGuid() {
+						return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
+					}
+
+					var urlIsString = match.url.indexOf('REGEXP') === -1;
+					var guid = makeGuid();
+
+					match.guid = guid;
+					match.url = match.url.replace('REGEXP', '');
+					
+					console.log('[FAKE XHR] Match added [' + (match.method || 'All REST verbs') + "] : " + match.url + "'");
+
+					match.requests = [];
+					match.responses = [];
+					window._ajaxmock_.call[guid] = match;
+
+					window._ajaxmock_.matches.push(function (method, url) {
+
+						if (!url) {
+							return false;
+						}
+
+						var urlMatch = urlIsString ? url.indexOf(match.url) !== -1 : new RegExp(match.url).test(url);
+						var methodMatch = (typeof match.method === 'undefined') ? true : match.method.toLowerCase() === method.toLowerCase();
+
+						if (urlMatch && methodMatch) {
+							return match;
+						} else {
+							return false;
+						}
+					});
+
+					return guid;
+				},
+				init: function () {
+					var _xhr = window.sinon.useFakeXMLHttpRequest();
+
+					_xhr.onCreate = function (request) {
+
+						setTimeout(function () {
+							var anyMatches = false;
+							var requests = window._ajaxmock_.requests;
+
+							if (!request.url) {
+								console.log('NO XHR URL');
+								return;
+							} // this shouldn't happen, but sometimes does
+							// store the request for later matching
+							if (requests[request.method.toLowerCase() + request.url]) {
+								requests[request.method.toLowerCase() + request.url].count++;
+							} else {
+								requests[request.method.toLowerCase() + request.url] = {
+									data: request,
+									count: 1
+								};
+							}
+
+							window._ajaxmock_.matches.forEach(function (func) {
+								anyMatches = anyMatches || func(request.method, request.url);
+							});
+
+							if (anyMatches) {
+
+								respond(request, anyMatches);
+
+							} else {
+								console.log('[FAKE XHR] did not respond to ' + request.url);
+							}
+						}, 100);
+					};
+				}
+			};
+			window._ajaxmock_.init();
+		}
+
+		function respond(request, response) {
+			var call = window._ajaxmock_.call;
+			var callForThisMatch;
+			var responseForThisMatch;
+			var status;
+			var body;
+
+			console.log('[FAKE XHR] gonna respond to ' + request.method + ": " + request.url + "'");
+
+			callForThisMatch = call[response.guid];
+			callForThisMatch.requests.push(request);
+
+			responseForThisMatch = callForThisMatch.responses[callForThisMatch.requests.length];
+
+			if (responseForThisMatch) {
+				status = responseForThisMatch.status;
+				body = responseForThisMatch.responseBody;
+			}
+
+			request.respond(
+			status || response.status || 200, response.headers || {
+				"Content-Type": "application/json"
+			}, body || response.responseBody || '');
+		}
+	});
+}
+
+function fake(options){
+
+	var url = !! options.url.source ? 'REGEXP' + options.url.source : options.url;
+	var guid = page.evaluate(function (url, method, responseBody, status) {
+		if (window._ajaxmock_ && url) {
+
+			if (responseBody) {
+				try {
+					JSON.parse(responseBody);
+				} catch (e) {
+					return 'JSON';
+				}
+			}
+
+			console.log("sending mock response");
+			return window._ajaxmock_.fake({
+				url: url,
+				method: method,
+				responseBody: responseBody,
+				status: status
+			});
+		}
+	}, url, options.method, options.responseBody, options.status );
+
+	if (guid === 'JSON') {
+		console.log('[FAKE FAILED] JSON was invalid : ' + options.method + ' : ' + url);
+	}
+
+	return {
+
+		count: function () {
+			return page.evaluate(function (guid) {
+				return window._ajaxmock_.call[guid].requests.length;
+			}, guid);
+		},
+
+		nthRequest: function (index) {
+			return page.evaluate(function (guid, index) {
+				var request = window._ajaxmock_.call[guid].requests[index - 1];
+				return request.requestBody;
+			}, guid, index);
+		},
+
+		last: function () {
+			var last = page.evaluate(function (guid) {
+				return window._ajaxmock_.call[guid].requests.length;
+			}, guid);
+
+			return this.nthRequest(last);
+		},
+
+		first: function () {
+			return this.nthRequest(1);
+		},
+
+		nthResponse: function (num, response) {
+			page.evaluate(function (guid, num, response) {
+				window._ajaxmock_.call[guid].responses[num] = response;
+			}, guid, num, response );
+
+			return this;
+		}
+	};
+}
+
+function getAllRequests() {
+	var requests = page.evaluate(function () {
+		var requests = {};
+
+		if (window._ajaxmock_) {
+			requests = window._ajaxmock_.requests;
+		}
+
+		return requests;
+	});
+
+	return requests;
+}
